ef8c450b746b6762a5b9f8071e8a3f3e14b28538
League Student <league@iMac-15.attlocal.net>
2019-11-09 17:28:27 -0800

    guessing pi and snowflake

--- src/_04_guessing_pi/GuessingPi.java
+++ src/_04_guessing_pi/GuessingPi.java
@@ -14,12 +14,10 @@ public class GuessingPi {
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-String pie = "3.141592653589793238";
+String pie = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518";
 		// 3. Print out the first 3 digits of Pi to the console.
 
-System.out.println(pie.charAt(0));
-System.out.println(pie.charAt(1));
-System.out.println(pie.charAt(2));
+
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
 
@@ -29,20 +27,47 @@ System.out.println(pie.charAt(2));
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-for(int i=0;i< 19; i++ ) {
+for(int i=0;i<19; i++ ) {
 	
 
 				// 5. Ask the user for the NEXT digit of pi.
+String v =JOptionPane.showInputDialog(null ,"what is the next digit of pie?");
 
 				// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
+if(v.equals(pie.charAt(i)+"")) {
+	System.out.println("correct");
+	
+}
 
-					// 7. If they are correct, print out "correct".
+else {
+	System.out.println("incorrect, start over");
+	break;
+}
+				
+      // 7. If they are correct, print out "correct".
 
 					// 8. If they are not, print out "incorrect" and tell them
 			//    to start over. Use 'break;' to break out of the loop.
 
 }
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/_05_snowflake/Snowflake.java
+++ src/_05_snowflake/Snowflake.java
@@ -6,25 +6,28 @@ package _05_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+Robot bob = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
+bob.setX(300);
+bob.setY(300);
 
 		// #3. Put the robot's pen down
 
-
+bob.penDown();
 		// #4. Set the robot’s speed to 5
-
+bob.setSpeed(5);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
+For(int)
 			
 			// #6. Set the pen color to black
 
