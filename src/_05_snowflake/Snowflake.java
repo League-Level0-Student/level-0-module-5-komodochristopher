a028196f57f19609fd60a872973d6f9664aef050
League Student <league@iMac-15.attlocal.net>
2019-11-16 17:27:28 -0800

    snowflake bob

--- src/_05_snowflake/Snowflake.java
+++ src/_05_snowflake/Snowflake.java
@@ -8,55 +8,63 @@ import java.awt.Color;
 
 import org.jointheleague.graphical.robot.Robot;
 
-
 public class Snowflake {
-	
+
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-Robot bob = new Robot();
+		Robot bob = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-bob.setX(300);
-bob.setY(300);
+		bob.setX(300);
+		bob.setY(300);
 
 		// #3. Put the robot's pen down
 
-bob.penDown();
+		bob.penDown();
 		// #4. Set the robot’s speed to 5
-bob.setSpeed(5);
+		bob.setSpeed(5);
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-For(int)
-			
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
+		for (int i = 0; i < 6; i++) {
+
 			// #6. Set the pen color to black
-
-			
+			bob.setPenColor(Color.black);
 			// #7. move the robot by 50
 
-			
+			bob.move(50);
 			// #8. turn the robot to the right by 60
-			
-			
+			bob.turn(60);
+
 			// #12. set the pen color to blue for even numbers of i
 
-			
-			// #13. set the pen color to red for all odd numbers of i
-			
-			
-			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
-		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
-			// #14. set the pen color to black
-				
-		
+			if(i%1 ==0) {
+			bob.setPenColor(Color.blue);
+		}
+		// #13. set the pen color to red for all odd numbers of i
+if(i%2==0) {
+	bob.setPenColor(Color.red);
+}
+		// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
+
+		for (int j = 0; j < 10; j++) {
+
+			// #10. turn the robot to the left by 60
+			bob.turn(60);
+
+			// #11. move the robot by 50 (play the start button)
+			bob.move(50);
+		}
+
+		// #14. set the pen color to black
+		bob.setPenColor(Color.black);
+		}
+		}
+
+	private static void For(Class<Integer> class1) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
