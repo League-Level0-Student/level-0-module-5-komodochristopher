f93f1b1e48adbf3619f162f9d6eaed3838132a55
League Student <league@iMac-15.attlocal.net>
2020-01-04 17:27:27 -0800

    Create answerkey.java

---
+++ src/answerkey.java
@@ -0,0 +1,50 @@
+
+public class answerkey {
+	
+public static void main(String[] args) {
+	
+
+	for(int i=1;i<100; i++) {
+		if(i%2==1) {
+		System.out.println(i);
+		System.out.println("odd");
+		}
+	
+		
+		}
+	
+	
+	
+	
+	
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
