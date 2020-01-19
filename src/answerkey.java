ff7b7ec18437af568ae9c4f3beaf512194682845
League Student <league@iMac-27.attlocal.net>
2020-01-18 17:24:07 -0800

    answerkey and test

--- src/answerkey.java
+++ src/answerkey.java
@@ -1,50 +1,18 @@
 
 public class answerkey {
-	
-public static void main(String[] args) {
-	
 
-	for(int i=1;i<100; i++) {
-		if(i%2==1) {
-		System.out.println(i);
-		System.out.println("odd");
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 501; i++) {
+			System.out.print(i);
+			if (i % 2 == 1) {
+				System.out.println("is odd");
+			} else {
+				if (i % 2 == 0) {
+					System.out.println("is even ");
+				}
+			}
 		}
-	
-		
-		}
-	
-	
-	
-	
-	
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	}
 }
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
