558e9ee6b14d3d85d1c71079ee1accd41569dce4
League Student <league@iMac-12.attlocal.net>
2020-01-25 17:25:07 -0800

    gauntlet

--- src/forloopgauntlet.java
+++ src/forloopgauntlet.java
@@ -1,46 +1,22 @@
 
 public class forloopgauntlet {
-public static void main(String[] args) {
-	
-	
-	for(int i=0;i<100; i++) {
-		System.out.println()
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 3; i++) {
+
+			for (int n = 0; n < 3; n++) {
+				System.out.print(i);
+				System.out.print(" ");
+			
+
+				System.out.println(n);
+				
+			}
+
+		}
+		
+		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+
 }
