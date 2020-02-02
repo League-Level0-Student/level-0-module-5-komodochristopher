e961f36439400e2e1fcc5d8dfd52d5c40eebfae6
League Student <league@iMac-12.attlocal.net>
2020-02-01 17:24:28 -0800

    gauntlet ip

--- src/anothergauntlet.java
+++ src/anothergauntlet.java
@@ -1,132 +1,19 @@
 
 public class anothergauntlet {
-public static void main(String[] args) {
-	
-	for( int i=0 ; i<9;i++) {
-		
-		
+	public static void main(String[] args) {
+int e =1;
+		for (int i = 0; i < 10; i++) {
+			
+			for (int p = 0; p < 10; p++) {
+				System.out.print(e);
+				System.out.print(" ");
+			
+				 e += 1;
+			}
+			System.out.println();
+		}
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
