d1b644a6c350028c75e2011ee0272e9bd5bada66
League Student <league@iMac-12.attlocal.net>
2020-02-14 16:56:23 -0800

    skillpractice

--- src/skillpracticeworksheet.java
+++ src/skillpracticeworksheet.java
@@ -14,24 +14,45 @@ public class skillpracticeworksheet {
 	void skill1() {
 		// Use pop-ups for the following.
 		// Ask the user how many dimes they have
-		String i = JOptionPane.showInputDialog("how many dimes do you have? ");
+		//String i = JOptionPane.showInputDialog("how many dimes do you have? ");
 		// Tell them how many cents they have (hint multiply by 10)
-int dimes = Integer.parseInt(i);
+//int dimes = Integer.parseInt(i);
 
-JOptionPane.showMessageDialog(null,dimes*10);
+//JOptionPane.showMessageDialog(null,dimes*10);
 
 // Ask the user how tall they are (inches)
- JOptionPane.showInputDialog("");
-		// If they are shorter than 36 inches, tell them to eat their Wheaties
+ //String y = JOptionPane.showInputDialog("how tall are you in inches");
 
+ //int whale =Integer.parseInt(y);
+ 
+ //if(whale<36) {
+	 //JOptionPane.showMessageDialog( null,"Eat more of your wheaties, bud");
+	 
 	}
+ 
+ 
+ 
+ // If they are shorter than 36 inches, tell them to eat their Wheaties
+
+	//}
 
 	void skill2() { // Write a loop to print every third number between 1 and 30 to the console
-
+for(int i=1;i<31;i+=3) {
+	System.out.println(i);
+	//if() {
+		
 	}
+}
+
+		
+		
+	
+	
+	
+	
 
 	void skill3() { // Get a random number that is less than 20 and print it to the console
-
+ //random randy = new random 
 		// Get another random number that is less than 10 and print it to the console
 
 		// Using a pop-up, tell the user the difference between the numbers // Hint: use
@@ -40,9 +61,10 @@ JOptionPane.showMessageDialog(null,dimes*10);
 	}
 
 	void skill4() { // In a pop-up, ask the user for the city they live in
-
+String click =JOptionPane.showInputDialog("What City do you live in?");
 		// If they answered "San Diego", tell them they live in America's Finest City
-
+int u =Integer.parseInt(click);
+if();                                       
 		// Otherwise, tell them to move to San Diego
 
 		// Create a variable - cars - and initialize it to the number of cars your
@@ -60,6 +82,5 @@ JOptionPane.showMessageDialog(null,dimes*10);
 
 		// In another pop-up, tell the user, that their school is a fantastic school. //
 		// You must include the name of the school in the message.
-
-	}
+		}
 }
